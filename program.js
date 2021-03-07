497fd01cba568e215106fc2bafa0831ff4b82285
coelho-faminto <coelho@nostracasa.org>
2021-03-07 13:03:19 -0300

    alpha version

--- program.js
+++ program.js
@@ -4,13 +4,69 @@ import System.IO;
 
 package JScriptNETProgram {
     class Program {
-        static function Main() {
-            var arrBytes: Byte[] = File.ReadAllBytes("program.js");
+        static function getNtOptionalHeaderMagic(arrBytes: Byte[]): Int16 {
+            var int_offset_e_lfanew = 0x3C;
+            var arr_e_lfanew: Byte[] = [
+                arrBytes[int_offset_e_lfanew],
+                arrBytes[int_offset_e_lfanew + 1],
+                arrBytes[int_offset_e_lfanew + 2],
+                arrBytes[int_offset_e_lfanew + 3]
+            ];
+            
+            var e_lfanew: Int32 = BitConverter.ToInt32(arr_e_lfanew, 0);
 
-            Console.WriteLine(arrBytes.Length);
-            Console.WriteLine("Test...");
+            return BitConverter.ToInt16(
+                [
+                    arrBytes[e_lfanew + 0x18],
+                    arrBytes[e_lfanew + 0x19]
+                ],
+                0
+            );
+        }
+
+        static function usage(argc: Int32, argv: System.String[]) {
+            if (argc != 2) {
+                Console.WriteLine("Usage: PEImageDetector.exe <file>");
+                return false;
+            }
+
+            if (!File.Exists(argv[1])) {
+                Console.WriteLine("File " + argv[1] + " not found.");
+                return false;
+            }
+
+            return true;
+        }
+
+        static function Main(argc: Int32, argv: System.String[]) {
+            if (!usage(argc, argv)) return;
+
+            Console.WriteLine('Verifying file "' + argv[1] + '"...');
+            
+            var arrBytes: Byte[] = File.ReadAllBytes(
+                argv[1]
+            );
+
+            Console.WriteLine('File has ' + arrBytes.Length + ' bytes.');
+
+            var wordMagic = getNtOptionalHeaderMagic(arrBytes);
+                
+            Console.WriteLine("Magic: " + wordMagic.toString(16));
+
+            switch (wordMagic) {
+                case 0x010B:
+                    Console.WriteLine("Image is PE32.");
+                    break;
+                case 0x020B:
+                    Console.WriteLine("Image is PE32+/PE64.");
+                    break;
+                default:
+                    Console.WriteLine("Unknown magic.");
+                    break;
+            }
         }
     }
 }
 
-JScriptNETProgram.Program.Main();
+var cmdLine = Environment.GetCommandLineArgs();
+JScriptNETProgram.Program.Main(cmdLine.Length, cmdLine);
